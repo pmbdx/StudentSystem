14d03766bedbdca5598295c2a395d7e8f61d402f
David Alejandro Elizarraraz Garcia <al220437@ULSA-DF.LASALLE.MX>
2026-03-02 21:43:11 -0600

    delete function

--- p1.js
+++ p1.js
@@ -83,6 +83,40 @@ function s_delete()
 	document.getElementById("s_report").style.display = "none";
 }
 
+function deleteData()
+{
+	ID = document.getElementById("IDB").value;
+	i = 0; //index
+	flag = false;
+	x = xmlDoc.getElementsByTagName("student");
+	l = x.length;
+	while ((i < l) && (flag==false))
+	{
+		if(x[i].childNodes[0].childNodes[0].nodeValue == ID)
+		{
+			userconf = confirm("Delete student : "
+								+x[i].childNodes[0].childNodes[0].nodeValue //ID
+								+" - "
+								+x[i].childNodes[1].childNodes[0].nodeValue //name
+								+"?"
+							);
+			if(userconf == true)
+			{
+				x[i].parentNode.removeChild(x[i]);
+				alert("Student deleted");
+			}
+				flag = true;
+		
+		}//if 
+		else
+		{
+			i++;	
+	
+		}//else
+	}//while
+	if(flag == false)
+		alert("Student not found");
+}//deleteData
 function s_update()
 {
 	document.getElementById("s_create").style.display = "none";
@@ -99,14 +133,59 @@ function s_search()
 	document.getElementById("s_search").style.display = "block";
 	document.getElementById("s_update").style.display = "none";
 	document.getElementById("s_report").style.display = "none";
-}
+}//s_search
 
+function search()
+{
+	field = document.getElementById("fieldSearch").value;
+	value =  document.getElementById("textSearch").value;
+	count = 0;
+
+	a = xmlDoc.getElementsByTagName("student");
+	x = xmlDoc.getElementsByTagName(field);
+	l = a.length;
+
+	var table = document.getElementById("resultSearch");
+
+	table.innerHTML = "";
+	table.innerHTML = "<thead><tr>"
+						+"<th>ID</th>"
+						+"<th>Name</th>"
+						+"<th>Sex</th>"
+						+"<th>Marital Status</th>"
+						+"</tr></thead>"
+						+"<tbody>";
+	for(i=0;i<l;i++)
+	{
+		if (x[i].childNodes[0].nodeValue == value)
+		{
+			table.innerHTML += "<tr>"
+							+"<td>"
+							+ a[i].childNodes[0].childNodes[0].nodeValue
+							+"</td>"
+							+"<td>"
+							+ a[i].childNodes[1].childNodes[0].nodeValue	
+							+"</td>"
+							+"<td>"
+							+ a[i].childNodes[2].childNodes[0].nodeValue	
+							+"</td>"
+							+"<td>"
+							+ a[i].childNodes[3].childNodes[0].nodeValue	
+							+"</td>"
+							+"</tr>";
+		}
+	}//for
+	table.innerHTML += "</tbody>";	
+
+
+
+}//search
 function s_report()
 {
-	document.getElementById("altas").style.display = "none";
-	document.getElementById("bajas").style.display = "none";
-	document.getElementById("buscar").style.display = "none";
-	document.getElementById("cambios").style.display = "none";
+	document.getElementById("s_create").style.display = "none";
+	document.getElementById("s_delete").style.display = "none";
+	document.getElementById("s_search").style.display = "none";
+	document.getElementById("s_update").style.display = "none";
 	document.getElementById("s_report").style.display = "block";
 
 	var table = document.getElementById("tableReport");
